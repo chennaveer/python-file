3b2071968780221c6a2377bf285be4c6a6e5f847
chennaveer <48007523+chennaveer@users.noreply.github.com>
2019-02-28 15:10:23 +0530

    Add files via upload

---
+++ cc.py
@@ -0,0 +1,18 @@
+import random
+
+d = 0
+p = 0
+
+while true:
+    r = input("press r to roll,q to quit: ")
+    
+    if r == 'r':
+    	d = random.randint
+        print("you got:",random.randomint(1,6))
+
+	elif d == 'q':
+         print("bye!")
+         exit() 
+
+
+print("end")
