a1e98baa951eb5eded393599fa7ed76b42494d74
chennaveer <48007523+chennaveer@users.noreply.github.com>
2019-03-12 10:57:07 +0530

    Add files via upload

---
+++ nikhil..py
@@ -0,0 +1,42 @@
+#creating a function no parameters
+def func1():
+	print("hii")
+
+func1()
+
+
+#with paramewters
+def func2(a):
+	print("hii"+"\t"+a)
+
+func2("nikhil")
+
+#with 2 parameters
+def func3(a,b):
+	c=a+b
+	print(c)
+
+func3(3,8)
+
+	
+
+#with 2 \3 parameters
+def func4(a,b,c):
+	d=a+b+c
+
+	print(d)
+
+func4(1,2,4)
+
+#with defalt parameters
+def fun5(univarcity='IITB'):
+	print("i an in "+ "\t" +university)
+
+func5(IITB)
+fun5(IITG)
+func5()
+
+
+
+
+	
